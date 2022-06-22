38b6a7a9ddd160306d31254bd3caccb83f77e551
leandroyabut <leandro.yabut@smoothstack.com>
2022-06-22 17:43:00 -0600

    Angular day 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {UselessFact} from './useless-fact.interface';
+import {ClipboardService} from 'ngx-clipboard';
+import {ToastrService} from 'ngx-toastr';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
-  title = 'may-2022';
+export class AppComponent implements OnInit{
+
+  apiUrl = "https://uselessfacts.jsph.pl/random.json?language=en";
+  uselessFact: UselessFact = {
+    id: '', language: '', permalink: '', source: '', source_url: '', text: ''
+  }
+
+  constructor(private http: HttpClient,
+              private clipboard: ClipboardService,
+              private toastr: ToastrService) {
+  }
+
+  ngOnInit(): void {
+    this.loadFact();
+  }
+
+  loadFact() {
+    this.http.get<UselessFact>(this.apiUrl)
+      .subscribe(
+        data => {
+          this.uselessFact = data;
+        }
+      );
+  }
+
+  copyFactUrl() {
+    this.clipboard.copy(this.uselessFact.permalink);
+    this.toastr.success("Copied useless fact to clipboard!");
+  }
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {ClipboardModule} from 'ngx-clipboard';
+import {ToastrModule} from 'ngx-toastr';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        HttpClientModule,
+        ClipboardModule,
+        BrowserAnimationsModule,
+        ToastrModule.forRoot()
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
