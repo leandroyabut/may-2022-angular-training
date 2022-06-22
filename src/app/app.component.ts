cdb7b96893bbb437d62447525ebfc83896edb2b5
leandroyabut <leandro.yabut@smoothstack.com>
2022-06-22 17:47:48 -0600

    Add eslint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,39 +6,39 @@ import {ToastrService} from 'ngx-toastr';
 
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.sass']
 })
 export class AppComponent implements OnInit{
 
-  apiUrl = "https://uselessfacts.jsph.pl/random.json?language=en";
-  uselessFact: UselessFact = {
-    id: '', language: '', permalink: '', source: '', source_url: '', text: ''
-  }
+	apiUrl = 'https://uselessfacts.jsph.pl/random.json?language=en';
+	uselessFact: UselessFact = {
+		id: '', language: '', permalink: '', source: '', source_url: '', text: ''
+	};
 
-  constructor(private http: HttpClient,
+	constructor(private http: HttpClient,
               private clipboard: ClipboardService,
               private toastr: ToastrService) {
-  }
+	}
 
-  ngOnInit(): void {
-    this.loadFact();
-  }
+	ngOnInit(): void {
+		this.loadFact();
+	}
 
-  loadFact() {
-    this.http.get<UselessFact>(this.apiUrl)
-      .subscribe(
-        data => {
-          this.uselessFact = data;
-        }
-      );
-  }
+	loadFact() {
+		this.http.get<UselessFact>(this.apiUrl)
+			.subscribe(
+				data => {
+					this.uselessFact = data;
+				}
+			);
+	}
 
-  copyFactUrl() {
-    this.clipboard.copy(this.uselessFact.permalink);
-    this.toastr.success("Copied useless fact to clipboard!");
-  }
+	copyFactUrl() {
+		this.clipboard.copy(this.uselessFact.permalink);
+		this.toastr.success('Copied useless fact to clipboard!');
+	}
 
 
 
